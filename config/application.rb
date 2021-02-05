6956694dcb8fe0d7ff0b2855617406e8e26af64e
rich88b <rich88b@gmail.com>
2021-02-04 20:58:39 -0600

    change aplication for load paths in prod

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,7 @@ module CodeChallenge
     config.autoload_paths += %W(#{Rails.root}/lib)
     config.autoload_paths += %W(#{Rails.root}/services)
 
-    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
-    config.eager_load_paths += Dir["#{config.root}/lib/formatter/**/"]
-    config.eager_load_paths += Dir["#{config.root}/services/**/"]
+    config.eager_load_paths += Dir["#{config.root}/lib"]
+    config.eager_load_paths += Dir["#{config.root}/services"]
   end
 end
